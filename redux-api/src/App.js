1f5c0778a3ac56befd0f32a7a776862c211758a2
Zolton <gmzesq@gmail.com>
2019-07-25 14:53:08 -0400

    Files set up

--- redux-api/src/App.js
+++ redux-api/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { connect } from "react-redux";
+import { FETCH_FAIL, FETCH_SUCCESS, FETCH_START, fetchRick } from "./action/action";
+import ReactDOM from "react-dom"
 
-function App() {
-  return (
-    <div className="App">
-      Hello from App
-    </div>
-  );
+function App(props) {
+  
+  return <div className="App">
+    Hello from App
+    {console.log(props)}
+    {props.rick ? console.log("state is filled", props) : <h1> Loading, please wait</h1>}
+    </div>;
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    error: state.error,
+    isFetching: state.isFetching,
+    rick: state.rick
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { FETCH_FAIL, FETCH_SUCCESS, FETCH_START, fetchRick }
+)(App);
---
+++ redux-api/src/action/action.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+
+export const fetchRick = () => dispatch => {
+  dispatch({ type: FETCH_START });
+  axios
+    .get("https://rickandmortyapi.com/api/character/")
+    .then(
+      res => (
+        console.log(res),
+        dispatch({
+          type: FETCH_SUCCESS,
+          payload: res.data
+        })
+      )
+    )
+    .catch(
+      error => (
+        console.log(error),
+        dispatch({
+          type: FETCH_FAIL,
+          payload: error.response
+        })
+      )
+    );
+};
